7d3014072e04256b5de5c11945cc9811e862f030
Apple <Apple@AppletekiMacBook-Pro-2.local>
2015-01-25 10:58:46 +0800

    新建

---
+++ AcquisitionBus/HomViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomViewController.h
+//  AcquisitionBus
+//
+//  Created by Apple on 15/1/25.
+//  Copyright (c) 2015年 beauty-soft.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomViewController : UIViewController
+
+@end
